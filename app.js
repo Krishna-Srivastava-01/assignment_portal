eb0d0da9f2c50fc4b0ed93adbcd31a31e8c4e93d
Krishna-Srivastava-01 <krishnasrivastava0111@gmail.com>
2026-09-23 23:51:46 +0530

    post

--- app.js
+++ app.js
@@ -6,5 +6,22 @@ const express = require('express');
 const app = express();;
 app.use(express.json());
 
+app.post('/assignments', async(req, res)=>{
+    try{
+        const {title, deadline}=req.body;
+        let result = await pool.query(`
+            INSERT INTO assignments(title, deadline)
+            values($1, $2)
+            returning *;
+            `, [title, deadline])
+        res.status(201).json(result.rows[0])
+    }catch(err){
+        console.log(err)
+        res.status(500).json({err:"Server Failed"});
+    }
+})
 
-app.listen(3000)
+
+app.listen(3000,() => {
+    console.log('Server is working')
+});
